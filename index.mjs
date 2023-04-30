2fd020a6bdd436549e28c5a53f77817c88c6af4e
Tushar Rai <tusharrai132@gmail.com>
2023-04-30 16:35:23 +0530

    boiler plate

---
+++ index.mjs
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+const app = express();
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+res.send("Hello World");
+
+})
+
+
+
+let dbURI = process.env.MONGO_URL
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+    console.log("mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log("mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+//   SIGINT  it means agar koi ctrl se close kr raha hai server ko to mongoose ko bhi close krdo
+process.on('SIGINT', () => {
+    console.log("App is terminating");
+    mongoose.connection.close(() => {
+        console.log("Mongoose default connection closed");
+        process.exit(0);
+    })
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("Listening on port 5000");
+})
