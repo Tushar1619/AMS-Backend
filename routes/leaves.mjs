bbeb23624192cd07124b5b8eda0f08f4ae8f4ade
Tushar Rai <tusharrai132@gmail.com>
2023-05-02 22:16:01 +0530

    token integrated 1

--- routes/leaves.mjs
+++ routes/leaves.mjs
@@ -1,41 +1,54 @@
-import  express  from "express";
+import express from "express";
 const router = express.Router();
 import { body, validationResult } from 'express-validator';
 import lettersModel from '../models/letter.mjs'
 
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 dotenv.config();
 
-router.post('/addleave',[
-    body('sub','Subject length should be greater than 5 characters').isLength({min: 6}),
-    body('desc','Description length should be greater than 10 characters').isLength({min: 11}),
+import jwt from 'jsonwebtoken'
+const jwt_key = 'jwtsecret';
+
+router.post('/addleave', [
+    body('sub', 'Subject length should be greater than 5 characters').isLength({ min: 6 }),
+    body('desc', 'Description length should be greater than 10 characters').isLength({ min: 11 }),
 ],
-async (req,res)=>{
-    const {sub,desc,url,t_id}=req.body;
-    const errors = validationResult(req);
+    async (req, res) => {
+        const { sub, desc, url, t_id } = req.body;
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
 
         }
 
-        try{
+        try {
 
-            let leaveLetter={
+            const token = req.header('auth-token')
+            console.log(token);
+            if (!token) {
+                return res.status(401).send("Please authenticate with valid auth-token")
+            }
+
+            const data = jwt.verify(token, jwt_key);
+            console.log(data);
+            let leaveLetter = {
+                user:data.user.id,
                 enrollmentNo: t_id,
                 subject: sub,
                 description: desc,
                 // url: url,
                 agreed: false,
             }
-            console.log(leaveLetter);
+            // console.log(leaveLetter);
+
             let createdLetter = await lettersModel.create(leaveLetter);
             res.send(createdLetter);
         }
-        catch(err){
+        catch (err) {
             console.log(err.message);
             res.status(500).send("Internal server error!");
         }
-}
+    }
 )
 
 export default router;
